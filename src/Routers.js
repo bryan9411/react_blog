f9f34b8c988de521ae326d71e3daec3d5f05b65b
Bryan9411 <66744004+Bryan9411@users.noreply.github.com>
2021-04-08 02:38:23 +0800

    Add files via upload

---
+++ src/Routers.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
+import App from 'components/App';
+import Login from 'pages/Login';
+import Register from 'pages/Register';
+import NotFound from 'pages/Notfound';
+import PostPage from 'pages/PostPage';
+import { AuthContext, LoadingContext } from 'Contexts';
+import { getMe } from 'WebApi';
+import { getAuthToken } from 'utils';
+
+export default function Routers() {
+  const [user, setUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  useEffect(() => {
+    if (getAuthToken()) {
+      getMe().then((res) => {
+        if (res.ok) {
+          setUser(res.data);
+        }
+      });
+    }
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user, setUser }}>
+      <Router>
+        <LoadingContext.Provider value={{ isLoading, setIsLoading }}>
+          <Switch>
+            <Route path="/" component={App} exact />
+            <Route path="/login" component={Login} />
+            <Route path="/register" component={Register} />
+            <Route path="/posts/:slug" component={PostPage} />
+            <Route component={NotFound} />
+          </Switch>
+        </LoadingContext.Provider>
+      </Router>
+    </AuthContext.Provider>
+  );
+}
